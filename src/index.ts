b495f5ccbb6967646189f10e5f8dc714ebb2493b
AnthocDev <anthocdev@gmail.com>
2021-03-14 14:09:54 +0000

    TypeORM/Basic entity/Migration config/DotEnv variables

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("It works.");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConf from "./mikro-orm.config";
+
+require("dotenv").config(); //Env variables access
+
+const main = async () => {
+  /*Establish DB connection */
+  const orm = await MikroORM.init(microConf);
+  /* Run Migrations */
+  await orm.getMigrator().up();
+  /* Create new Post */
+  const post = orm.em.create(Post, { title: "test post" });
+  await orm.em.persistAndFlush(post);
+};
+
+main();
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,20 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import path from "path";
+require("dotenv").config(); //Env variables access
+
+console.log(process.env.DB_NAME);
+
+export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  entities: [Post],
+  dbName: process.env.DB_NAME,
+  type: "postgresql",
+  debug: !__prod__,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+} as Parameters<typeof MikroORM.init>[0];
