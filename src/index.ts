b6e2316e5318b224fd80468b794f837353fc0a05
AnthocDev <anthocdev@gmail.com>
2021-03-15 12:14:03 +0000

    Express + Apollo + GraphQL + Test Resolver - 47:00

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConf from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 require("dotenv").config(); //Env variables access
 
@@ -10,9 +14,27 @@ const main = async () => {
   const orm = await MikroORM.init(microConf);
   /* Run Migrations */
   await orm.getMigrator().up();
-  /* Create new Post */
-  const post = orm.em.create(Post, { title: "test post" });
-  await orm.em.persistAndFlush(post);
+  /* Server Set-Up */
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.get("/", (req, res) => {
+    res.send("Server UP");
+  });
+  app.listen(process.env.EXPRESS_PORT, () => {
+    console.log(
+      "Express server is UP on http://localhost:" +
+        process.env.EXPRESS_PORT +
+        "/graphql"
+    );
+  });
 };
 
 main();
