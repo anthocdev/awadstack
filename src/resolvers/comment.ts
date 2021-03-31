dc415523dc6aaf2d2b4063f3cfc010cd69aca640
AnthocDev <anthocdev@gmail.com>
2021-03-31 23:32:33 +0100

    Comment updating and deleting

--- src/resolvers/comment.ts
+++ src/resolvers/comment.ts
@@ -113,4 +113,65 @@ export class CommentResolver {
       relations: ["user"],
     });
   }
+  /* Update Comment */
+  @Mutation(() => UserComment, { nullable: true })
+  @UseMiddleware(isAuth)
+  async updateComment(
+    @Arg("commentId", () => Int) commentId: number,
+    @Arg("input") input: CommentInput,
+    @Ctx() { req }: MyContext
+  ): Promise<UserComment | undefined> {
+    /* Look for the comment that matches both id and user ID (ensure privilege to edit) */
+    const comment = await UserComment.findOne({
+      id: commentId,
+      userId: req.session.userId,
+    });
+
+    if (!comment) {
+      return undefined;
+    }
+
+    if (typeof input !== "undefined") {
+      /* Double checking userID (might be unecessary)*/
+      await UserComment.update(
+        { id: commentId, userId: req.session.userId },
+        { body: input.body }
+      );
+    }
+
+    /* Returning updated comment with likes/dislikes */
+    const updatedComment = await getConnection()
+      .getRepository(UserComment)
+      .createQueryBuilder("comment")
+      .where(`comment.id = ${commentId}`)
+      .loadRelationCountAndMap("comment.likes", "comment.ratings", "rc", (qb) =>
+        qb.where("rc.rating = true")
+      )
+      .loadRelationCountAndMap(
+        "comment.dislikes",
+        "comment.ratings",
+        "rc",
+        (qb) => qb.where("rc.rating = false")
+      )
+      .getOne();
+
+    return updatedComment;
+  }
+
+  /* Delete Comment */
+  @Mutation(() => Boolean)
+  @UseMiddleware(isAuth)
+  async deleteMovie(
+    @Arg("commentId") commentId: number,
+    @Ctx() { req }: MyContext
+  ): Promise<boolean> {
+    try {
+      /* Deleting comment that matches both id and user id within session (to check privilege) */
+      await UserComment.delete({ id: commentId, userId: req.session.userId });
+    } catch {
+      return false;
+    }
+
+    return true;
+  }
 }
